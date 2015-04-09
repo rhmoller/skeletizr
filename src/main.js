1da32a165009b296280cb51b978f59278a762f21
Rene Hangstrup Møller <rhmoller@gmail.com>
2015-04-09 21:49:07 +0200

    a bad tree

--- src/main.js
+++ src/main.js
@@ -75,7 +75,76 @@ preloader.load(function (assets) {
   bones.push(bone10);
   bones.push(bone11);
 
+  bone1.syncShape();
+
+  insertTree();
+
   var manipulator = new Manipulator(sheet, bones);
   manipulator.select(bone1);
 
 });
+
+class TreeNode {
+
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+    this.expanded = false;
+  }
+
+  addChild(child) {
+    this.children.push(child);
+  }
+}
+
+function renderNode(sb, node, level) {
+  let icon;
+  if (node.children.length == 0) {
+    icon = "fa fa-square-o";
+  } else {
+    icon = (node.expanded) ? "fa fa-minus-square-o" : "fa fa-plus-square-o";
+  }
+
+  let display = (node.expanded) ? "block" : "none";
+  sb.push(`<div class=node><i class='${icon}'></i><span class=label>${node.name}</span>`);
+  sb.push(`<div class='children' style='display: ${display};'>`);
+  for (let child of node.children) {
+    renderNode(sb, child, level + 1);
+  }
+  sb.push("</div>");
+  sb.push("</div>");
+}
+
+function insertTree() {
+  let tree = new TreeNode("Root");
+  let body = new TreeNode("Body");
+  let leftArm = new TreeNode("Left arm");
+  let leftHand = new TreeNode("Left hand");
+  let rightArm = new TreeNode("Right arm");
+  let rightHand = new TreeNode("Right hand");
+
+  tree.addChild(body);
+  body.addChild(leftArm);
+  body.addChild(rightArm);
+  leftArm.addChild(leftHand);
+  rightArm.addChild(rightHand);
+
+  let nav = document.getElementById("navigator");
+  let sb = [];
+
+  sb.push("<div class=tree>");
+  renderNode(sb, tree, 0);
+  sb.push("</div>");
+
+  nav.innerHTML = sb.join("");
+  nav.addEventListener("click", (e) => {
+    let tgt = e.target;
+    if (tgt.className.indexOf("fa") >= 0 ||
+        tgt.className.indexOf("label") >= 0) {
+          let node = tgt.parentNode;
+          let old = node.children[2].style.display;
+          node.children[2].style.display = (old == "none") ? "block" : "none";
+    }
+  });
+
+}
